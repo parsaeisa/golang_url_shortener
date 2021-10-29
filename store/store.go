a66f68c0c9ecc8c94f2de1aad2d584dd2dfec2bb
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-10-30 00:53:55 +0330

    feature (store) : connecting to redis

--- store/store.go
+++ store/store.go
@@ -1,10 +1,38 @@
 package store
 
 import (
-	"github.com/go-redis/redis"
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/go-redis/redis/v8"
 )
 
 type Cache struct {
 	redisClient *redis.Client
+}
+
+var storeService = &Cache{}
+var c = context.Background()
+
+const TimeLimit = 24 * time.Hour
+
+func ConnectToRedis() *Cache {
+
+	// create redis connection
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:8000",
+		Password: "",
+		DB:       0,
+	})
+
+	//check redis connection
+	_, err := redisClient.Ping(c).Result()
+	if err != nil {
+		fmt.Sprintf("Redis failed : %v", err)
+	}
+
+	fmt.Printf("Redis successed")
+	storeService.redisClient = redisClient
+	return storeService
 }
